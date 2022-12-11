abac09c5d5f418eba3ede4133a7cb468b34ddd16
VISMA-DEV <visma-dev@mail.ru>
2022-12-11 16:30:40 +0300

    Displaying posts on the main page

--- app/include/recent.php
+++ app/include/recent.php
@@ -1,82 +1,36 @@
 <?php
 include ("app/controllers/categories.php");
 
+$posts = selectAll('posts', ['status' => 1]);//выводим все посты, кроме архивных.
 ?>
+
 <div class="container-main">
     <div class="content row">
 
         <!-- Posts -->
         <div class="main-content col-md-8 col-12">
             <h2>Последние:</h2>
-            <div class="post row">
-                <div class="post-text">
-                    <a href="#"><img src="/assets/img/post-1.webp" alt="post-photo"></a>
-                    <h3>
-                        <a href="#">Утечка: новый Samsung Unpacked пройдёт 10 августа. На нём покажут Galaxy Z Flip 4</a>
-                    </h3>
-                    <div class="data-icons">
-                        <i class="far fa-calendar"> 18 Июля</i>
-                        <i class="far fa-user"> Константин Воронин</i>
+            <?php foreach ($posts as $post): ?>
+                <div class="post row">
+                    <div class="post-text">
+                        <a href="<?=BASE_URL . 'single.php?post=' . $post['id'];?>"><img src="<?=BASE_URL . 'assets/img/posts/' . $post['img'];?>" alt="<?=$post['title'];?>"></a>
+                        <h3>
+                            <a href="<?=BASE_URL . 'single.php?post=' . $post['id'];?>"><?=mb_substr($post['title'], 0, 200, 'UTF-8');?></a>
+                        </h3>
+                        <div class="data-icons">
+                            <i class="far fa-calendar"> <?=substr($post['date'], 0, 10)?></i>
+                            <!-- выводим имя автора, делая запрос к бд через id -->
+                            <?php $author = selectOne('users', ['id' => $post['author_id']]);?>
+                            <i class="far fa-user"><?=' '. $author['username'];?></i>
+                        </div>
+                        <p class="preview-text"><?=mb_substr($post['content'], 0, 200, 'UTF-8') . '...';?></p>
+                        <i class="fa-solid fa-heart like action-icons"></i>
+                        <i class="fa-solid fa-heart-crack dislike action-icons"></i>
+                        <i class="fa-solid fa-comment action-icons"></i>
+                        <i class="fa-solid fa-share action-icons"></i>
                     </div>
-                    <p class="preview-text">Инсайдер @evleaks опубликовал в Twitter рекламный постер, на котором написано, что следующее мероприятие Samsung Galaxy Unpacked стартует 10 августа 2022 года.</p>
-                    <i class="fa-solid fa-heart like action-icons"></i>
-                    <i class="fa-solid fa-heart-crack dislike action-icons"></i>
-                    <i class="fa-solid fa-comment action-icons"></i>
-                    <i class="fa-solid fa-share action-icons"></i>
                 </div>
-            </div>
-            <div class="post row">
-                <div class="post-text">
-                    <a href="#"><img src="/assets/img/post-2.jpg" alt="post-photo"></a>
-                    <h3>
-                        <a href="#">Китайский кроссовер OMODA C5 выйдет в России в октябре</a>
-                    </h3>
-                    <div class="data-icons">
-                        <i class="far fa-calendar"> 18 Июля</i>
-                        <i class="far fa-user"> Виталий Лейкин</i>
-                    </div>
-                    <p class="preview-text">В июне этого года компания АО «Чери автомобили рус» объявила о расширении своего присутствия в России и анонсировала запуск нового бренда OMODA. Тогда же сообщили, что первым в продажу поступит кроссовер OMODA C5, но сроков не назвали.</p>
-                    <i class="fa-solid fa-heart like action-icons"></i>
-                    <i class="fa-solid fa-heart-crack dislike action-icons"></i>
-                    <i class="fa-solid fa-comment action-icons"></i>
-                    <i class="fa-solid fa-share action-icons"></i>
-                </div>
-            </div>
-            <div class="post row">
-                <div class="post-text">
-                    <a href="#"><img src="/assets/img/post-3.jpg" alt="post-photo"></a>
-                    <h3>
-                        <a href="#">Odyssey Team анонсировала скорый релиз утилиты для джейлбрейка iOS 15</a>
-                    </h3>
-                    <div class="data-icons">
-                        <i class="far fa-calendar"> 18 Июля</i>
-                        <i class="far fa-user"> Генадий Никель</i>
-                    </div>
-                    <p class="preview-text">Команда Odyssey Team, которая уже выпускала решения по джейлбрейку предыдущих версий iOS, заявила о скором релизе утилиты для взлома iOS 15.</p>
-                    <i class="fa-solid fa-heart like action-icons"></i>
-                    <i class="fa-solid fa-heart-crack dislike action-icons"></i>
-                    <i class="fa-solid fa-comment action-icons"></i>
-                    <i class="fa-solid fa-share action-icons"></i>
-                </div>
-            </div>
-            <div class="post row">
-                <div class="post-text">
-                    <a href="#"><img src="/assets/img/post-4.jpg" alt="post-photo"></a>
-                    <h3>
-                        <a href="#">В Китае вышел Xiaomi F22 Pro — «король кнопочных телефонов» с Android 12</a>
-                    </h3>
-                    <div class="data-icons">
-                        <i class="far fa-calendar"> 17 Июля</i>
-                        <i class="far fa-user"> Владимир Сухой</i>
-                    </div>
-                    <p class="preview-text">На китайском рынке вышел Xiaomi F22 Pro — «король кнопочных телефонов». Именно так устройство описывает производитель.</p>
-                    <i class="fa-solid fa-heart like action-icons"></i>
-                    <i class="fa-solid fa-heart-crack dislike action-icons"></i>
-                    <i class="fa-solid fa-comment action-icons"></i>
-                    <i class="fa-solid fa-share action-icons"></i>
-                </div>
-            </div>
-
+            <?php endforeach; ?>
         </div>
         <!-- Sidebar -->
         <div class="sidebar col-md-4 col-12">
@@ -90,14 +44,11 @@ include ("app/controllers/categories.php");
             <div class="section topics">
                 <h3>Разделы:</h3>
                 <ul>
-                    <?php
-
-                    foreach ($categories as $key => $category): ?>
-                    <li><a href="#"><?=$category['name']; ?></a></li>
+                    <?php foreach ($categories as $key => $category): ?>
+                    <li><a href="#"><?=$category['name'];?></a></li>
                     <?php endforeach; ?>
                 </ul>
             </div>
-
         </div>
     </div>
 </div>
